3c8ee16b41500110fffa7f41b82032ee224ae6d1
taismith <tasmith8@aggies.ncat.edu>
2019-10-08 09:18:41 -0400

    Initial Commit

---
+++ Tip Calculator App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator App
+//
+//  Created by Tai Smith on 10/8/19.
+//  Copyright © 2019 Tai Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
